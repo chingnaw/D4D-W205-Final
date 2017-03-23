66765b80f447b17c4008ab97fa4eef1be74c4518
divyasriram22 <divyasriram@berkeley.edu>
2017-03-22 23:25:03 -0700

    reuploading script to change headers file names

---
+++ script_rename_t_headers_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#1
+# ONLY LINE THAT NEEDS TO BE CHANGED FOR SCRIPTING
+#PLEASE CHANGE STATE ABBREVIATION TO CURRENT STATE WHOSE DATA IS BEING USED
+
+state=oh
+
+end_of_file=2010_t_headers.csv
+
+name_of_this_file=$0
+
+for file in *; do
+        if [ file!=$0 ]; then
+                #changing the header file name to match the data file name for each state table
+                table_value=`echo $file | cut -d \_ -f1`
+		#echo $table_value
+		#echo $state"_"$table_value"_"$end_of_file".csv"
+                mv $file $state"_"$table_value"_"$end_of_file".csv"
+        fi
+done
+
